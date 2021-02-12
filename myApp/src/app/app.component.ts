6e42139850fc32699079330c81fb769bbbc6fafe
Francesco <fre@eagleprojects.it>
2021-02-12 10:34:44 +0100

    PannAdminFrex1

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -3,12 +3,38 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
- 
+
+export class AppComponent { 
+
+  usermobile: string;
+  progetto: string;
+  usermobile1: string;  
+  progetto1: string;
+  usermobile2: string;  
+  progetto2: string;
+
+  constructor(private http: HttpClient) {  
+
+    //http.get('http://www.collaudolive.com:9087/test.php')
+    http.get('http://www.collaudolive.com:9082/')
+    //http.get('/9082')
+    //http.get('http:/localhost:9082/')
+    .subscribe(res=> {      
+      this.usermobile = res[0]['usermobile'];
+      this.progetto = res[0]['progetto'];
+      this.usermobile1 = res[1]['usermobile'];
+      this.progetto1 = res[1]['progetto'];
+      this.usermobile2 = res[2]['usermobile'];
+      this.progetto2 = res[2]['progetto'];
+    }); 
+    
+  }
+
 }
--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +19,7 @@ import { ServersComponent } from './servers/servers.component';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule {
+
+}
