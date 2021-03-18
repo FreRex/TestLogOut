5464ec3b39ee42baf540cf2018710dc43bcfa090
UBUNTU <fre@eagleprojects.it>
2021-03-18 11:01:45 +0100

    Riavvio video: ottimizzazione scrript

--- build/ctrl/controllerVidApp.js
+++ build/ctrl/controllerVidApp.js
@@ -1,25 +1,28 @@
 "use strict";
 exports.VidApp = (req, res, next) => {
-    console.log('ok vid app !!!');
     const { exec } = require("child_process");
-    exec("sudo pm2 restart app", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stdout) {
-            console.log(`stdout: ${stdout}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        try {
-            res.send('riavvio effettuato');
-        }
-        catch (error) {
-            res.send('Errore: riavvio non effettuato');
-        }
-    });
+    try {
+        exec("sudo pm2 restart app", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stdout) {
+                console.log(`stdout: ${stdout}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+        });
+        res.json({
+            "restartNMS": true
+        });
+    }
+    catch (error) {
+        res.json({
+            "restartNMS": false
+        });
+    }
 };
