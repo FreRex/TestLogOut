90ae3d0b4a9a7fe8c334056923a8969a944c5e4e
Daniele Bambini <dbambini@eagleprojects.it>
2021-03-19 12:33:04 +0100

    crea e cancella utenti

--- frontend/src/app/backoffice/users/create-user-modal/create-user-modal.component.ts
+++ frontend/src/app/backoffice/users/create-user-modal/create-user-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-create-user-modal',
@@ -8,13 +10,63 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreateUserModalComponent implements OnInit {
 
+  form:FormGroup;
+
+
   constructor(
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private userService: UserService
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.form = new FormGroup({
+      collaudatoreufficio: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(30)]
+      }),
+      username: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(50)]
+      }),
+      password: new FormControl( null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(30)]
+      }),
+      autorizzazioni: new FormControl( null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(1)]
+      }),
+
+    });
+
+
+  }
 
   closeModal(){
     this.modalCtrl.dismiss(CreateUserModalComponent);
+  }
+
+  createUser(){
+    if (!this.form.valid) { return; }
+    this.userService
+      .addUser(
+        this.form.value.collaudatoreufficio,
+        this.form.value.username,
+        this.form.value.password,
+        +this.form.value.autorizzazioni)
+      .subscribe(
+        res => {
+          // console.log("Response",res);
+          // this.presentToast('Room creata!');
+          this.form.reset();
+          this.modalCtrl.dismiss({ message: 'user create' }, 'save');
+        },
+        // (err: HttpErrorResponse) => {
+        //   console.log("Error:", err.error['text']);
+        //   this.createErrorAlert(err.error['text']);
+        // }
+      );
+
   }
 }
--- frontend/src/app/backoffice/users/user.service.ts
+++ frontend/src/app/backoffice/users/user.service.ts
@@ -5,7 +5,7 @@ import { tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
 import { environment } from 'src/environments/environment';
 
-export interface User{
+export interface User {
   collaudatoreufficio: string;
   username: string;
   password: string;
@@ -15,35 +15,90 @@ export interface User{
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   private usersSubj = new BehaviorSubject<User[]>([]);
   users$: Observable<User[]> = this.usersSubj.asObservable();
 
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService
-  ) { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  // loadUsers(){
-  //   this.http
-  //     .get<User[]>(
-  //       'https://www.collaudolive.com:9083/s/utenti/'
-  //     ).subscribe(
-  //       users =>{
+  loadUsers(){
+    this.http
+      .get<User[]>(
+        'https://www.collaudolive.com:9083/s/utenti/',{
+          headers: new HttpHeaders().set(
+            'Authorization',
+            `Bearer ${this.authService.token}`
+          ),
+        }
+      ).subscribe(
+        users =>{
+          this.usersSubj.next(users);
+        }
+      )
+  }
+
+  // loadUsers(): Observable<User[]> {
+  //   return this.http
+  //     .get<User[]>(`${environment.apiUrl}/s/utenti/`, {
+  //       headers: new HttpHeaders().set(
+  //         'Authorization',
+  //         `Bearer ${this.authService.token}`
+  //       ),
+  //     })
+  //     .pipe(
+  //       tap((users) => {
   //         this.usersSubj.next(users);
-  //       }
-  //     )
+  //       })
+  //     );
   // }
 
-  loadUsers(): Observable<User[]> {
-    return this.http
-      .get<User[]>(
-        `${environment.apiUrl}/s/utenti/`,
-        { headers: new HttpHeaders().set('Authorization', `Bearer ${this.authService.token}`) }
-      )
-      .pipe(tap(users =>{ this.usersSubj.next(users); }));
+  addUser(
+    collaudatoreufficio: string,
+    username: string,
+    password: string,
+    autorizzazioni: number
+  ) {
+    return this.http.post(
+      `${environment.apiUrl}/cu/`,
+      {
+        collaudatoreufficio: collaudatoreufficio,
+        username: username,
+        password: password,
+        autorizzazioni: autorizzazioni
+      },
+
+      {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          `Bearer ${this.authService.token}`
+        ),
+      }
+    ).pipe(tap(res=>{
+      this.loadUsers();
+
+    }))
+    ;
   }
+
+  deleteUser(userId:number){
+    return this.http.post(
+      `${environment.apiUrl}/d/`,
+      {
+        "id" : userId,
+        "tableDelete" : "utenti"
+      },
+      {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          `Bearer ${this.authService.token}`
+        ),
+      }
+    ).pipe(tap(res=>{
+      this.loadUsers();
+    }));
+
+  }
+
 }
