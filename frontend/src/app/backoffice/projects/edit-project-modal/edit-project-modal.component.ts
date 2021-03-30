54dd5441647892da4e610f6609d451e03478fc3b
Daniele Bambini <dbambini@eagleprojects.it>
2021-03-30 11:02:31 +0200

    coordinate okProj

--- frontend/src/app/backoffice/projects/edit-project-modal/edit-project-modal.component.ts
+++ frontend/src/app/backoffice/projects/edit-project-modal/edit-project-modal.component.ts
@@ -38,22 +38,19 @@ export class EditProjectModalComponent implements OnInit {
           updateOn: 'blur',
           validators: [Validators.required, Validators.maxLength(50)],
         }),
-        pk_proj: new FormControl(this.project.pk_proj, {
-          updateOn: 'blur',
-          validators: [Validators.required, Validators.maxLength(50)],
-        }),
         nome: new FormControl(this.project.nome, {
           updateOn: 'blur',
           validators: [Validators.required, Validators.maxLength(50)],
         }),
-        long_centro_map: new FormControl(this.project.long_centro_map, {
+        coordinate: new FormControl( `${this.project.long_centro_map}, ${this.project.lat_centro_map}`, {
           updateOn: 'blur',
           validators: [Validators.required, Validators.maxLength(50)],
         }),
-        lat_centro_map: new FormControl(this.project.lat_centro_map, {
+
+/*         lat_centro_map: new FormControl(this.project.lat_centro_map, {
           updateOn: 'blur',
           validators: [Validators.required, Validators.maxLength(50)],
-        }),
+        }), */
 /*         nodi_fisici: new FormControl(this.project.nodi_fisici, {
           updateOn: 'blur',
           validators: [Validators.required, Validators.maxLength(50)],
@@ -87,14 +84,17 @@ export class EditProjectModalComponent implements OnInit {
     if (!this.form.valid) {
       return;
     }
+
+    const coords = this.form.value.coordinate.split(",");
+
     this.projectService
       .updateProject(
         this.project.idprogetto,
         this.form.value.idutente,
         this.form.value.pk_proj,
         this.form.value.nome,
-        this.form.value.long_centro_map,
-        this.form.value.lat_centro_map,
+        coords[0],
+        coords[1],
       ).subscribe(res=>{
         this.presentToast("Progetto Aggiornato");
         this.form.reset();
