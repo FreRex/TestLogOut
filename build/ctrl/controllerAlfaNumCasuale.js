4241c7c895ff734331e470fb0454a241411678fe
UBUNTU <fre@eagleprojects.it>
2021-04-23 09:21:46 +0200

    Eliminazione backend compilato

--- build/ctrl/controllerAlfaNumCasuale.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.getAlfaNumeCasuale = (req, res, next) => {
-    const codcasuale = require('.././middleware/alfaNumeCasuale');
-    let codcas = codcasuale.alfaNumeCasuale(10);
-    console.log('Codice casuale univoco: ' + codcas);
-    res.json(codcas);
-};
--- build/ctrl/controllerTest.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = (req, res, next) => {
-    const codcasutente = require('.././middleware/idCasUtenti');
-    let codicecasualeutente = codcasutente.idCasUtenti(8);
-    console.log("ciaoo: " + codicecasualeutente);
-};
