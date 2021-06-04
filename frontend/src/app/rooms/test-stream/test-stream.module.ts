a5af78e7c258248a7adfb1237fc5dee4a0649837
mgiannotti <mgiannotti@eagleprojects.it>
2021-06-04 17:12:32 +0200

    separato componenti player e webcam

--- frontend/src/app/rooms/test-stream/test-stream.module.ts
+++ frontend/src/app/rooms/test-stream/test-stream.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
+import { PlayerComponent } from './player/player.component';
 import { TestStreamPageRoutingModule } from './test-stream-routing.module';
 import { TestStreamPage } from './test-stream.page';
+import { WebcamComponent } from './webcam/webcam.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, TestStreamPageRoutingModule],
-  declarations: [TestStreamPage],
+  declarations: [TestStreamPage, PlayerComponent, WebcamComponent],
 })
 export class TestStreamPageModule {}
--- frontend/src/app/rooms/test-stream/test-stream.page.ts
+++ frontend/src/app/rooms/test-stream/test-stream.page.ts
@@ -1,17 +1,11 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Storage } from '@capacitor/storage';
 import { NavController } from '@ionic/angular';
-import FlvJs from 'flv.js';
 import { Socket } from 'ngx-socket-io';
 import { from, Subscription } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
-
-const height = 120;
-const width = 120;
-const framerate = 15;
-const audiobitrate = 44100;
 
 @Component({
   selector: 'app-test-stream',
@@ -21,18 +15,11 @@ const audiobitrate = 44100;
 export class TestStreamPage implements OnInit, AfterViewInit, OnDestroy {
   private sub: Subscription;
 
-  @ViewChild('local_video', { static: false }) localVideo: ElementRef;
-  @ViewChild('output_video', { static: false }) remoteVideo: ElementRef;
-
   roomId: string = '';
   userId: string = '';
 
   rtmpDestination: string = '';
   flvOrigin: string = '';
-
-  private localStream: MediaStream;
-  private mediaRecorder: MediaRecorder;
-  private constraints: MediaStreamConstraints;
 
   constructor(
     private socket: Socket,
@@ -64,7 +51,6 @@ export class TestStreamPage implements OnInit, AfterViewInit, OnDestroy {
         this.userId = userId;
         this.rtmpDestination = `${environment.urlRTMP}/${this.roomId}/${this.userId}`;
         this.flvOrigin = `${environment.urlWSS}/${this.roomId}/${this.userId}.flv`;
-        this.listaDispositivi();
         this.configureSocketMessageHandler();
       });
   }
@@ -97,189 +83,6 @@ export class TestStreamPage implements OnInit, AfterViewInit, OnDestroy {
       },
       (err) => console.log(err)
     );
-  }
-
-  private async requestGetUserMedia(devicePosition): Promise<void> {
-    if (devicePosition == 'fronte') {
-      this.constraints = {
-        audio: { sampleRate: audiobitrate, echoCancellation: true },
-        video: {
-          width: { min: 100, ideal: width, max: 1920 },
-          height: { min: 100, ideal: height, max: 1080 },
-          frameRate: { ideal: framerate },
-        },
-      };
-    } else if (devicePosition == 'retro') {
-      this.constraints = {
-        audio: { sampleRate: audiobitrate, echoCancellation: true },
-        video: {
-          width: { min: 100, ideal: width, max: 1920 },
-          height: { min: 100, ideal: height, max: 1080 },
-          frameRate: { ideal: framerate },
-          facingMode: { exact: 'environment' },
-        },
-      };
-    }
-    this.localStream = await navigator.mediaDevices.getUserMedia(this.constraints);
-  }
-
-  startLocalVideo(): void {
-    this.localStream.getTracks().forEach((track) => {
-      track.enabled = true;
-    });
-    this.localVideo.nativeElement.srcObject = this.localStream;
-  }
-
-  stopLocalVideo() {
-    this.localStream.getTracks().forEach((track) => {
-      track.enabled = false;
-    });
-    this.localVideo.nativeElement.srcObject = undefined;
-  }
-
-  async startStreming(devicePosition): Promise<void> {
-    try {
-      this.requestGetUserMedia(devicePosition).then((res) => {
-        this.startLocalVideo();
-        this.configureMediaRecorder();
-        this.socket.emit('start', 'start');
-      });
-    } catch (err) {
-      this.closeVideoCall();
-    }
-    return null;
-  }
-
-  stopStreaming(): void {
-    this.socket.emit('hangup', '');
-    this.closeVideoCall();
-  }
-
-  private closeVideoCall(): void {
-    if (this.mediaRecorder) {
-      this.mediaRecorder.stop();
-      this.mediaRecorder.onstop = null;
-      this.mediaRecorder.onstart = null;
-      this.mediaRecorder.onpause = null;
-      this.mediaRecorder.onresume = null;
-      this.mediaRecorder.onerror = null;
-      this.mediaRecorder.ondataavailable = null;
-      this.mediaRecorder = null;
-    }
-    this.stopLocalVideo();
-    this.socket.emit('disconnect', '');
-  }
-
-  configureMediaRecorder() {
-    this.mediaRecorder = new MediaRecorder(this.localStream);
-
-    this.mediaRecorder.onstop = this.handleMediaRecorderOnStopEvent;
-    this.mediaRecorder.onstart = this.handleMediaRecorderOnStartEvent;
-    this.mediaRecorder.onpause = this.handleMediaRecorderOnPauseEvent;
-    this.mediaRecorder.onresume = this.handleMediaRecorderOnResumeEvent;
-    this.mediaRecorder.onerror = this.handleMediaRecorderOnErrorEvent;
-    this.mediaRecorder.ondataavailable = this.handleMediaRecorderOnDataAvailableEvent;
-
-    this.mediaRecorder.start(250);
-  }
-
-  handleMediaRecorderOnStopEvent = (event: Event) => {
-    console.log('media recorder stopped: ', event);
-  };
-  handleMediaRecorderOnStartEvent = (event: Event) => {
-    console.log('media recorder started: ', event);
-  };
-  handleMediaRecorderOnPauseEvent(event: Event) {
-    console.log('media recorder paused: ', event);
-  }
-  handleMediaRecorderOnResumeEvent = (event: Event) => {
-    console.log('media recorder resumed: ', event);
-  };
-  handleMediaRecorderOnErrorEvent = (event: MediaRecorderErrorEvent) => {
-    console.log('error', event.error);
-  };
-  handleMediaRecorderOnDataAvailableEvent = (event: BlobEvent) => {
-    console.log('🐱‍👤 : TestStreamPage : event.data', event.data);
-    // this.socket.emit('message', {type: 'binarystream', data: event.data});
-    this.socket.emit('binarystream', event.data);
-  };
-
-  /************************* flvplayer *************************/
-
-  idVarEglassesInFunction;
-  idVarVideoZoomInFunction;
-
-  player: FlvJs.Player;
-
-  startFlvPlayer() {
-    if (typeof this.player !== 'undefined') {
-      if (this.player != null) {
-        this.player.unload();
-        this.player.detachMediaElement();
-        this.player.destroy();
-        this.player = null;
-      }
-    }
-    this.player = FlvJs.createPlayer(
-      {
-        type: 'flv',
-        url: this.flvOrigin,
-      },
-      {
-        enableWorker: false,
-        enableStashBuffer: false,
-        stashInitialSize: 1,
-        isLive: true,
-        autoCleanupSourceBuffer: true,
-      }
-    );
-    this.player.attachMediaElement(this.remoteVideo.nativeElement);
-    this.player.load();
-    this.player.play();
-  }
-
-  stopFlvPlayer() {
-    if (this.player) {
-      this.player.pause();
-      this.player.unload();
-      this.player.detachMediaElement();
-      this.player.destroy();
-      this.player = null;
-    }
-  }
-
-  /************************* RetroFrontCam *************************/
-
-  checkRetroCam = false;
-
-  async listaDispositivi() {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-      console.log('enumerateDevices() not supported.');
-      return;
-    } else {
-      // List cameras and microphones.
-      await navigator.mediaDevices
-        .enumerateDevices()
-        .then(function (devices) {
-          const videoDevices = devices.filter((device) => device.kind === 'videoinput');
-          videoDevices.forEach(function (devices) {
-            //Check retro cam
-            if (
-              devices.label.indexOf('facing back') > 0 ||
-              /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-                navigator.userAgent
-              )
-            ) {
-              this.checkRetroCam = true;
-            }
-          });
-        })
-        .catch(function (err) {
-          console.log(err.name + ': ' + err.message);
-        });
-    }
-    console.log('retroCam:' + this.checkRetroCam);
-    return this.checkRetroCam;
   }
 }
 
